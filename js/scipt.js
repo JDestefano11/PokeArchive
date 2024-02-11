1c55f1222034a4ca5f66f71c6ac861ae8db5b467
DesJCodes <141986568+DesJCodes@users.noreply.github.com>
2024-02-11 12:53:00 -0500

    Formatted document

--- js/scipt.js
+++ js/scipt.js
@@ -3,3 +3,7 @@ alert('Hello world')
 let favortieFood = 'Tacos';
 favortieFood = 'Tacos'
 document.write('Tacos')
+
+
+var message='Javascript\'s beauty is simplicity';
+document.write('Javascript's beauty is simplicity');
