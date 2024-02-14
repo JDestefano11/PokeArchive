e40c60d50e54620afa1e303b00d26f26096b8487
DesJCodes <141986568+DesJCodes@users.noreply.github.com>
2024-02-13 19:17:08 -0500

    Cleared js file to start project

--- js/scipt.js
+++ js/scipt.js
@@ -1,9 +1,1 @@
-alert('Hello world')
 
-let favortieFood = 'Tacos';
-favortieFood = 'Tacos'
-document.write('Tacos')
-
-
-var message='Javascript\'s beauty is simplicity';
-document.write('Javascript's beauty is simplicity');
