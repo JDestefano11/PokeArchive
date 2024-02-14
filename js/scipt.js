d28c23615cb3d1d6baa4db2d3c737d43b81fc25f
DesJCodes <141986568+DesJCodes@users.noreply.github.com>
2024-02-13 21:50:23 -0500

    Added functionality to display 9 Pokemon with their names, heights, and types in the JavaScript Pokedex.

--- js/scipt.js
+++ js/scipt.js
@@ -1,1 +1,12 @@
+const pokemonList = [
+    pokemon1 { name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison'] }
+    pokemon2 { name: 'Ivysaur', height: 1, type: ['Grass', 'Poison'] }
+    pokemon3 { name: 'Venusaur', height: 2, type: ['Grass', 'Poison'] }
+    pokemon4 { name: 'Charmander', height: 0.6, type: ['Fire'] }
+    pokemon5 { name: 'Charmeleon', height: 1.1, type: ['Fire'] }
+    pokemon6 { name: 'Charizard', height: 1.7, type: ['Fire'] }
+    pokemon7 { name: 'Squirtle', height: 0.5, type: ['Water'] }
+    pokemon8 { name: 'Wartortle', height: 1, type: ['Water'] }
+    pokemon9 { name: 'Blastoise', height: 1.6, type: ['Water'] }
+]
 
