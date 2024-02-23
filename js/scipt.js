11846acb177e823c033002fad2861dcf85d59542
DesJCodes <141986568+DesJCodes@users.noreply.github.com>
2024-02-23 13:44:51 -0500

    git commit -m "Enclosed code inside an IFIE" "Refactored forEach loop"
    Wrapped the existing code inside an Immediately Invoked Function Expression
    
    -`getAll`: returns all pokemon in the array.
    -`add`:  adds a pokemon to the pokemon list as long as the condition is met.
    -`printPokemonDetails`: function that prints all the pokemons details
    
    Updated forEach loop so there is full access to iterate over the pokemonList.

--- js/scipt.js
+++ js/scipt.js
@@ -1,19 +1,40 @@
-let pokemonList = [
-    { name: 'Bulbasaur', height: 0.7, types: ['Grass', 'Poison'] },
-    { name: 'Ivysaur', height: 1, types: ['Grass', 'Poison'] },
-    { name: 'Venusaur', height: 2, types: ['Grass', 'Poison'] },
-    { name: 'Charmander', height: 0.6, types: ['Fire'] },
-    { name: 'Charmeleon', height: 1.1, types: ['Fire'] },
-    { name: 'Charizard', height: 1.7, types: ['Fire', 'Flying'] },
-    { name: 'Squirtle', height: 0.5, types: ['Water'] },
-    { name: 'Wartortle', height: 1, types: ['Water'] },
-    { name: 'Blastoise', height: 1.6, types: ['Water'] },
-];
+let pokemonRepository = (function () {
 
-function printPokemonDetails(pokemon) { 
-    console.log(pokemon.name + '  Height: ' + pokemon.height + '  Types: ' + pokemon.types.join(', '));
-}
-pokemonList.forEach(printPokemonDetails); 
+    let pokemonList = [
+        { name: 'Bulbasaur', height: 0.7, types: ['Grass', 'Poison'] },
+        { name: 'Ivysaur', height: 1, types: ['Grass', 'Poison'] },
+        { name: 'Venusaur', height: 2, types: ['Grass', 'Poison'] },
+        { name: 'Charmander', height: 0.6, types: ['Fire'] },
+        { name: 'Charmeleon', height: 1.1, types: ['Fire'] },
+        { name: 'Charizard', height: 1.7, types: ['Fire', 'Flying'] },
+        { name: 'Squirtle', height: 0.5, types: ['Water'] },
+        { name: 'Wartortle', height: 1, types: ['Water'] },
+        { name: 'Blastoise', height: 1.6, types: ['Water'] },
+    ];
 
+    function printPokemonDetails(pokemon) {
+        console.log(pokemon.name + '  Height: ' + pokemon.height + '  Types: ' + pokemon.types.join(', '));
+    }
 
+    // Returns all pokemon in the array
+    function getAll() {
+        return pokemonList;
+    }
 
+    // Adds a pokemon to the pokemonList 
+    function add(item) {
+        if (typeof item === 'object' && 'name' in item && 'height' in item && 'types' in item) {
+            pokemonList.push(item);
+        } else {
+            console.error('Invalid pokemon object')
+        }
+    }
+    return {
+        getAll: getAll,
+        add: add,
+        printPokemonDetails: printPokemonDetails
+    };
+
+    // IFIE
+})();
+pokemonRepository.getAll().forEach(pokemonRepository.printPokemonDetails);
