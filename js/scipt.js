a6a94169527a18480fd2c10bfa07260bb484a98e
DesJCodes <141986568+DesJCodes@users.noreply.github.com>
2024-02-27 14:54:51 -0500

    feat: Add addListItem function and improve code structure
    - Introduce addListItem function to encapsulate the creation of list items and buttons for each Pokémon.
    - Refactor the forEach loop to use addListItem for better code organization.
    - Fix a parameter name error in the add function.
    - Update styles.css for better responsiveness on different screen sizes.

--- js/scipt.js
+++ js/scipt.js
@@ -22,19 +22,36 @@ let pokemonRepository = (function () {
     }
 
     // Adds a pokemon to the pokemonList 
-    function add(item) {
-        if (typeof item === 'object' && 'name' in item && 'height' in item && 'types' in item) {
-            pokemonList.push(item);
+    function add(pokemon) {
+        if (typeof pokemon === 'object' && 'name' in pokemon && 'height' in pokemon && 'types' in pokemon) {
+            pokemonList.push(pokemon);
         } else {
-            console.error('Invalid pokemon object')
+            console.error('Invalid pokemon object');
         }
     }
+
+    // Adds a list item for a pokemon to the pokemonListItems
+    function addListItem(pokemon) {
+        let pokemonListItems = document.querySelector('.pokemon-list');
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add('pokemon-button');
+        listItem.appendChild(button);
+        pokemonListItems.appendChild(listItem);
+    }
+
     return {
         getAll: getAll,
         add: add,
-        printPokemonDetails: printPokemonDetails
+        printPokemonDetails: printPokemonDetails,
+        addListItem: addListItem
     };
 
     // IFIE
 })();
-pokemonRepository.getAll().forEach(pokemonRepository.printPokemonDetails);
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+});
+
